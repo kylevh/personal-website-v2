d6d0000a18f0de07bdb278a1292fa9683f33bef6
Kyle Huynh <kyle_huynh@hotmail.com>
2023-09-14 21:59:01 -0700

    homepage finish

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { sf } from "@/app/fonts";
+import Navbar from "@/components/ui/navbar"
 import Link from "next/link";
 
 export default function Home() {
@@ -10,82 +11,46 @@ export default function Home() {
   } else if (12 <= localClientTime && localClientTime < 18) {
     motd = "good afternoon."
   } else {
-	motd = "good evening."
+    motd = "good evening."
   }
 
   return (
-    <main className="flex min-h-screen items-center justify-between p-2 sm:p-24 bg-kyle-white">
-      <header className="text-gray-600 body-font hidden sm:block sm:fixed z-auto pt-4 h-screen w-full">
-        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <nav className="md:ml-auto flex flex-wrap items-center  mr-52 text-base justify-center">
-            <Link
-              href={"/works"}
-              className={`${sf.className} font-light mr-20 text-xl cursor-pointer text-gray-500 hover:text-kyle-dark duration-200`}
-            >
-              works
-            </Link>
-            <Link
-              href={"/info"}
-              className={`${sf.className} font-light mr-20 text-xl cursor-pointer text-gray-500 hover:text-kyle-dark duration-200`}
-            >
-              info
-            </Link>
-            <Link
-              href={"/contact"}
-              className={`${sf.className} font-light mr-20 text-xl cursor-pointer text-gray-500 hover:text-kyle-dark duration-200`}
-            >
-              contact
-            </Link>
-          </nav>
-        </div>
-      </header>
+    <main className="flex h-screen items-center justify-between overflow-hidden p-2 sm:p-24 bg-kyle-white">
+      {/* <Navbar/> */}
+      {/* Container for hero home page */}
+      <div className="grid grid-cols-3 lg:grid-cols-12 overflow-hidden p-4 gap-2 h-screen w-full ">
 
-	{/* Container for hero home page */}
-      <div className="grid grid-cols-3 lg:grid-cols-12 p-4 gap-2 h-auto w-full z-auto ">
-		{/* Left Header Text */}
-        <div className="w-full col-span-3 col-start-1 ">
-          <h1
-            className={`${sf.className} font-bold text-7xl sm:text-8xl sm:px-5 sm:mb-6 mb-2 text-left text-kyle-dark`}
-          >
-            kyle
-          </h1>
-          <h1
-            className={`${sf.className} font-bold text-7xl sm:text-8xl sm:px-5 sm:mb-6 mb-1 text-left text-kyle-dark`}
-          >
-            {" "}
-            huynh,
-          </h1>
-          <h1
-            className={`${sf.className} font-light text-7xl sm:text-8xl sm:px-5 text-left text-kyle-dark`}
-          >
-            designer
-          </h1>
-          <div className="h-[0rem]" />
+        <div className="w-full col-span-12 col-start-1">
+          <div className='h-20'>
+            <Navbar />
+          </div>
         </div>
 
-		{/* Right header text */}
-        <div className="w-full justify-center col-span-4 col-start-1 mt-16
-		
-		md:col-span-6 xl:mt-[18rem] 
-		xl:col-span-5 
-		bg-purple-700 sm:bg-red-700 md:bg-orange-400 lg:bg-yellow-200 xl:bg-green-700
-		">
-          <h1
-            className={`${sf.className} font-bold text-3xl sm:text-4xl text-left text-kyle-dark w-full`}
-          >
-            {"{"} {motd} {"}"}
-          </h1>
+        {/* Left Header Text */}
+        <div className="w-full h-auto col-span-3 col-start-1">
+          <h1 className={`${sf.className} font-bold text-7xl sm:text-8xl sm:px-0 sm:mb-6 mb-2 text-left text-kyle-dark`}> kyle </h1>
+          <h1 className={`${sf.className} font-bold text-7xl sm:text-8xl sm:px-0 sm:mb-6 mb-1 text-left text-kyle-dark`}> {" "} huynh, </h1>
+          <h1 className={`${sf.className} font-light text-7xl sm:text-8xl sm:px-0 text-left text-kyle-dark`} > designer </h1>
+          <div className="lg:h-64 h-0" />
+        </div>
 
-          <div className="w-12 h-[7px] col-span-2 col-start-6 my-6 bg-kyle-dark" />
-
-          <p
-            className={`${sf.className} font-light text-2xl sm:text-3xl text-left text-kyle-dark`}
-          >
+        {/* Right header text bg-purple-700 sm:bg-red-700 md:bg-orange-400 lg:bg-yellow-200 xl:bg-green-700 */}
+        <div className="w-full col-span-4 col-start-1 mt-0 lg:mt-28
+        lg:col-span-5 
+        lg:col-start-8
+        xl:col-start-9
+        
+        ">
+          <h1 className={`${sf.className} w-96 lg:mt-40 font-bold text-4xl xl sm:text-4xl text-left text-kyle-dark `} > {"{"} {motd} {"}"} </h1>
+          <div className="w-8 sm:w-12 h-[5px] sm:h-[7px] col-span-2 col-start-6 my-6 bg-kyle-dark" />
+          <p className={`${sf.className} md:w-96 font-light text-3xl sm:text-3xl text-left text-kyle-dark`}>
             i’m kyle, a multi-disciplinary designer and software engineer based
             in seattle, wa
           </p>
         </div>
       </div>
+
     </main>
   );
 }
+
---
+++ components/ui/navbar.tsx
@@ -0,0 +1,45 @@
+'use client'
+import React, { useState } from 'react'
+import Link from 'next/link'
+import { sf } from "@/app/fonts";
+
+
+
+function Navbar() {
+    const [menuOpen, setMenuOpen] = useState(false);
+    const toggleMenu = () => setMenuOpen(!menuOpen);
+
+    return (
+        <header className="text-gray-600  body-font block h-24 z-auto pt-4 w-full">
+            {/* Desktop navbar */}
+            <div className="container flex flex-wrap p-5 flex-col items-end justify-center lg:justify-end ">
+                <nav className="flex flex-wrap items-end text-base justify-end">
+                    <Link
+                        href={"/works"}
+                        className={`${sf.className} font-light mr-12 sm:mr-20 text-lg md:text-xl text-center justify-center cursor-pointer text-gray-500 hover:text-kyle-dark duration-200`}
+                    >
+                        works
+                    </Link>
+                    <Link
+                        href={"/info"}
+                        className={`${sf.className} font-light mr-12 sm:mr-20 text-lg md:text-xl cursor-pointer text-gray-500 hover:text-kyle-dark duration-200`}
+                    >
+                        info
+                    </Link>
+                    <Link
+                        href={"/contact"}
+                        className={`${sf.className} font-light lg:mr-20 text-lg md:text-xl cursor-pointer text-gray-500 hover:text-kyle-dark duration-200`}
+                    >
+                        contact
+                    </Link>
+                </nav>
+            </div>
+
+            {/* Mobile navbar */}
+            
+
+        </header>
+    )
+}
+
+export default Navbar
